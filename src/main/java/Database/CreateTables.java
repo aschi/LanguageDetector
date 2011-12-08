639ecd990377039d8cc9937fc1a8337cdec08548
Priscilla Schneider <schnepri@students.zhaw.ch>
2011-12-08 18:17:35 +0100

    Database

---
+++ src/main/java/Database/CreateTables.java
@@ -0,0 +1,114 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Diese Klasse erstellt die Tabelle Kunde. Achtung!!!!!! Sollten die
+ * entsprechende Tabelle bereits vorhanden sein, wird diese gelöscht und neu
+ * erstellt. Deshalb sollte diese Klasse mit Bedacht ausgeführt werden.
+ */
+public class CreateTables {
+
+	/**
+	 * Diese Methode erstellt die Tabellen Wort und Sprache. Achtung!!!!!!
+	 * Sollten die entsprechende Tabelle bereits vorhanden sein, werden diese
+	 * gelöscht und neu erstellt. Deshalb sollte diese Klasse mit Bedacht
+	 * ausgeführt werden.
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @throws Exception
+	 *             wenn die Connection mit der language-Datenbank nicht
+	 *             erfolgreich ist
+	 */
+	public void createTable() throws ClassNotFoundException, SQLException {
+
+		Class.forName("org.sqlite.JDBC");
+
+		Connection conn = DriverManager
+				.getConnection("jdbc:sqlite:language.db");
+		delete(conn);
+		create(conn);
+
+	}
+
+	public void delete(Connection conn) throws SQLException {
+		try {
+			Statement stmt = conn.createStatement();
+			String sql = "DROP TABLE 'word'";
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			Statement stmt = conn.createStatement();
+			String sql = "DROP TABLE 'language'";
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			Statement stmt = conn.createStatement();
+			String sql = "DROP TABLE 'languageWord'";
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void create(Connection conn) throws SQLException {
+
+		try {
+			Statement stmt = conn.createStatement();
+			String sql = "CREATE TABLE 'word' "
+					+ " (wordID integer PRIMARY KEY autoincrement," 
+					+ " word varchar(100)),"
+					+ " hitCount integer;";
+			stmt.execute(sql);
+
+			stmt = conn.createStatement();
+			sql = "CREATE TABLE 'language' "
+					+ " (languageID integer PRIMARY KEY autoincrement, "
+					+ " language varchar(50))" + ";";
+			stmt.execute(sql);
+			
+			stmt = conn.createStatement();
+			sql = "CREATE TABLE 'languageWord' "
+					+ " (lwID integer PRIMARY KEY autoincrement, "
+					+ " fkWordID integer," 
+					+ " fkLanguageID integer);";
+			stmt.execute(sql);
+		} finally {
+			if (conn != null)
+				conn.close();
+		}
+
+	}
+
+	/**
+	 * Führt createTablesInTierverwaltungDatabase aus. Siehe Javadoc von
+	 * createTablesInTierverwaltungDatabase.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CreateTables db = new CreateTables();
+		try {
+			db.createTable();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("fehlerli");
+		}
+	}
+
+}
---
+++ src/main/java/Database/DataMapper.java
@@ -0,0 +1,219 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import ch.hszt.LanguageDetector.Language;
+import ch.hszt.LanguageDetector.Word;
+
+public final class DataMapper {
+
+	private Connection conn;
+
+	public DataMapper() throws ClassNotFoundException, SQLException {
+
+		Class.forName("org.sqlite.JDBC");
+
+		conn = DriverManager.getConnection("jdbc:sqlite:language.db");
+	}
+
+	/**
+	 * Inserts a new word into table word
+	 * 
+	 * @param word
+	 *            Word to insert
+	 * @return int wordID
+	 * @throws SQLException
+	 */
+	public int insertWord(Word word) throws SQLException {
+
+		String sql;
+		ResultSet rs = null;
+		sql = "INSERT INTO 'word' VALUES (null, \'" + word.getText() + "\', " + word.getHitCount() + ");";
+		System.out.println(sql);
+		Statement stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+
+		PreparedStatement pstmt = conn
+				.prepareStatement("select max(wordID) max from 'word';");
+		rs = pstmt.executeQuery();
+		if (rs.next()) {
+			;
+			return rs.getInt("max");
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Inserts a new language into table language
+	 * 
+	 * @param language
+	 *            Language to insert
+	 * @return int languageID
+	 * @throws SQLException
+	 */
+	public int insertLanguage(String language) throws SQLException {
+
+		String sql;
+		ResultSet rs = null;
+		sql = "INSERT INTO 'language' VALUES (null, \'" + language + "\');";
+		System.out.println(sql);
+		Statement stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+
+		PreparedStatement pstmt = conn
+				.prepareStatement("select max(languageID) max from 'language';");
+		rs = pstmt.executeQuery();
+		if (rs.next()) {
+			;
+			return rs.getInt("max");
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Inserts a new word-language-relationshop into table wordLanguage
+	 * 
+	 * @param wordID
+	 *            the ID of the word to connect to a language
+	 * @param languageID
+	 *            the ID of the language to connect to a word
+	 * @throws SQLException
+	 */
+	public void insertWordLanguage(int wordID, int languageID)
+			throws SQLException {
+
+		String sql;
+		sql = "INSERT INTO 'wordLanguage' VALUES (null, " + wordID + ", "
+				+ languageID + ");";
+		System.out.println(sql);
+		Statement stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+	}
+
+	/**
+	 * Deletes all entities in tables word, language and wordLanguage
+	 * 
+	 * @throws SQLException
+	 */
+	public void delete() throws SQLException {
+		String sql;
+		sql = "DELETE * FROM 'word';";
+		System.out.println(sql);
+		Statement stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+
+		sql = "DELETE * FROM 'language';";
+		System.out.println(sql);
+		stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+
+		sql = "DELETE * FROM 'wordLanguage';";
+		System.out.println(sql);
+		stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+	}
+
+	/**
+	 * Returns all Words with all languages in a Set
+	 * 
+	 * @return a Set with all words
+	 * @throws SQLException
+	 */
+	public Set<Word> getWordSet() throws SQLException {
+
+		Map<Integer, String> languages = getLanguages();
+
+		String sql;
+		sql = "SELECT * FROM 'word';";
+		System.out.println(sql);
+
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		Set<Word> wordSet = new TreeSet<Word>();
+
+		while (rs.next()) {
+
+			Word word = new Word(rs.getInt("WordID"), rs.getString("word"), rs.getInt("hitCount"));
+			addLanguagesToWord(languages, word);
+
+			wordSet.add(word);
+		}
+		return wordSet;
+	}
+
+	private void addLanguagesToWord(Map<Integer, String> map, Word word) {
+		String sql;
+		sql = "SELECT * FROM 'wordLanguage' WHERE fkWordID = \'" + word.getId() + "\';";
+		System.out.println(sql);
+
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				Language l = new Language(rs.getInt("fkLanguageID"), map.get(rs.getInt("fkLanguageID")));
+				word.addLanguage(l);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private Map<Integer, String> getLanguages() {
+		Map<Integer, String> returnMap = new HashMap<Integer, String>();
+
+		String sql;
+		sql = "SELECT * FROM 'language';";
+		System.out.println(sql);
+		ResultSet rs;
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				returnMap
+						.put(rs.getInt("languageID"), rs.getString("language"));
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return returnMap;
+
+	}
+	
+	/**
+	 * Saves all words, languages and relationships between them in the tables
+	 * @param words all Words
+	 * @throws SQLException 
+	 */
+	public void saveAllWords(Set<Word> words, Set<Language> languages) throws SQLException {
+		
+		for (Language l: languages) {
+			insertLanguage(l.getLanguage());
+		}
+		
+		for (Word word : words) {
+			insertWord(word);
+			for (Language l: word.getLanguages()) {
+				insertWordLanguage(word.getId(), getLanguageID(l));
+			}
+		}
+	}
+
+}
